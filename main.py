e0098d6087d35c803e6da05d0a2210ecaf867e81
Andre Machado Eurochem <andre.machado@eurochemsam.com>
2024-11-04 17:33:40 -0300

    exercicios de dicionario

--- main.py
+++ main.py
@@ -3,6 +3,34 @@
 # nome: str = 'André'
 # is_estudante: bool = True
 
+# import json
+
+# lista: list = ['Sapato', 39, 10.38, True]
+
+# produto01: dict = {
+#     "nome": "Sapato",
+#     "quantidade": 39,
+#     "preco": 10.38,
+#     "disponibilidade": True
+# }
+
+# produto02: dict = {
+#     "nome": "Televisao",
+#     "quantidade": 10,
+#     "preco": 70.38,
+#     "disponibilidade": False
+# }
+
+# carrinho: list = []
+
+# carrinho.append(produto01)
+# carrinho.append(produto02)
+
+# print(carrinho)
+
+# carrinho_json = json.dumps(carrinho)
+# print(carrinho_json)
+
 # Desafio Tipar Código da aula Passada
 
 nome_valido: bool = False
@@ -48,3 +76,40 @@ bonus_recebido: float = 1000 + salario * bonus  # Exemplo simples de KPI
 
 # Imprime as informações para o usuário
 print(f"{nome}, seu salário é R${salario:.2f} e seu bônus final é R${bonus_recebido:.2f}.")
+
+### Crie dicionário para armazenar informações de um livro
+### Incluindo título, autor e ano de publicação. Imprima cada informação.
+
+from typing import Dict, Any
+
+livro_01: Dict[str, Any] = {
+    "Titulo": "Harry Potter e a Pedra Filosofal",
+    "Autor": "J.K Rolling",
+    "Ano": 2000
+}
+
+livro_02: Dict[str, Any] = {
+    "Titulo": "Harry Potter e a Câmara Secreta",
+    "Autor": "J.K Rolling",
+    "Ano": 2002
+}
+
+lista_de_livros = []
+
+lista_de_livros.append(livro_01)
+lista_de_livros.append(livro_02)
+
+print(lista_de_livros)
+
+lista_de_livros_usando_dict: dict = {
+    "livro_01": {"Titulo": "Harry Potter e a Pedra Filosofal",
+    "Autor": "J.K Rolling",
+    "Ano": 2000},
+
+    "livro_02": {"Titulo": "Harry Potter e a Câmara Secreta",
+    "Autor": "J.K Rolling",
+    "Ano": 2002}
+}
+
+print(lista_de_livros_usando_dict['livro_01'])
+print(lista_de_livros_usando_dict["livro_01"]['Titulo'])
